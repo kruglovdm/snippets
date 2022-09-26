c5f271b72d306d82540e68e7ba4c5948d6425025
Denis Kruglov <kruglovdm@mail.ru>
2022-09-26 20:02:43 +0300

    first

---
+++ Snippets/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from MainApp import views
+
+urlpatterns = [
+    path('', views.index_page, name='home'),
+    path('snippets/add', views.add_snippet_page, name='snippets-add'),
+    path('snippets/list', views.snippets_page, name='snippets-list'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
