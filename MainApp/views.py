37925830252b5d9dd1ffdbe3f10eb9fb933dcd84
Denis Kruglov <kruglovdm@mail.ru>
2022-09-27 22:53:35 +0300

    20220927

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,18 +1,25 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
-from MainApp.forms import SnippetForm
+from MainApp.forms import SnippetForm, UserRegistrationForm
+from django.contrib import auth
 
 
 def index_page(request):
-    context = {'pagename': 'PythonBin'}
+    context = {
+        'page_id': 'home',
+        'pagename': 'PythonBin',
+    }
     return render(request, 'pages/index.html', context)
 
 
 def add_snippet_page(request):
     if request.method == "GET":
         form = SnippetForm()
-        context = {'pagename': 'Добавление нового сниппета', "snippet_form": form}
+        context = {
+            'page_id': 'add_snippet',
+            'pagename': 'Добавление нового сниппета',
+            "snippet_form": form}
         return render(request, 'pages/add_snippet.html', context)
     elif request.method == "POST":
         form = SnippetForm(request.POST)
@@ -24,11 +31,48 @@ def add_snippet_page(request):
 
 def snippets_page(request):
     snippets = Snippet.objects.all()
-    context = {'pagename': 'Просмотр сниппетов', "snippets": snippets}
+    context = {
+        'page_id': 'view_snippets',
+        'pagename': 'Просмотр сниппетов',
+        "snippets": snippets,
+    }
     return render(request, 'pages/view_snippets.html', context)
 
 
 def snippet_detail(request, snippet_id):
     snippet = Snippet.objects.get(pk=snippet_id)
-    context = {'pagename': 'Страница сниппета', "snippet": snippet}
+    context = {
+        'page_id': 'snippet_page',
+        'pagename': 'Страница сниппета',
+        "snippet": snippet}
     return render(request, 'pages/snippet_page.html', context)
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+        else:
+            # Return error message
+            pass
+    return redirect('home')
+
+def logout_page(request):
+    auth.logout(request)
+    return redirect(request.META.get('HTTP_REFERER', '/'))
+
+def reg_page(request):
+    if request.method == "GET":
+        form = UserRegistrationForm()
+        context = {
+            'page_id': 'registration',
+            'pagename': 'Регистрация пользователя',
+            "form": form}
+        return render(request, 'pages/registration.html', context)
+    elif request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -3,10 +3,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from MainApp import views
 
+
 urlpatterns = [
     path('', views.index_page, name='home'),
     path('snippets/add', views.add_snippet_page, name='snippets-add'),
     path('snippets/list', views.snippets_page, name='snippets-list'),
     path('snippet/<int:snippet_id>', views.snippet_detail, name='snippet-detail'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_page, name='logout'),
+    path('reg/>', views.reg_page, name='reg'),
  ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
